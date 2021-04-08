97624672283dd8d6ce91d17a1d1b8ca1f76197c2
iLn0ur <70770292+iLn0ur@users.noreply.github.com>
2021-04-08 19:30:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+var Tc = 20
+var Tf = (9 / 5) * Tc + 32
+console.log(Tf)
+
+var name = 'Василий'
+var admin = name
+console.log(admin)
+
+var temp = 1000 + "108"
+console.log(temp)
+// 1000108
